dab6e2904395e53c63adc87a18cdc157ae612330
Wellight07 <tayyh07@gmail.com>
2026-04-28 23:50:56 +0800

    haven't test

--- backend.py
+++ backend.py
@@ -9,18 +9,18 @@ def home():
 if __name__ == "__main__":
     app.run(debug=True)
 
-#def calculate_balance():
- #   transactions = get_transactions()
-#
- #   balance = 0
-#
- #   for t in transactions:
-  #      t_type = t[0]
-   #     amount = t[1]
-#
- #       if t_type == "income":
-  #          balance += amount
-   #     else:
-    #        balance -= amount
-    #
-    #return balance
+def calculate_balance():
+    transactions = get_transactions()
+
+    balance = 0
+
+    for t in transactions:
+        t_type = t[0]
+        amount = t[1]
+
+        if t_type == "income":
+            balance += amount
+        else:
+            balance -= amount
+    
+    return balance
